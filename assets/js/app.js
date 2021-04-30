1c0c65f952afa3825d5ff167d3e094f70cb538f8
acktyne <pierremarc.baril@gmail.com>
2021-04-29 22:46:26 -0400

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-console.log('Hello world!')
+$(window).scroll(function() {     
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+        $("#sticky-header").addClass("active");
+    }
+    else {
+        $("#sticky-header").removeClass("active");
+    }
+});
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top -140
+    }, 500);
+    return false;
+});
